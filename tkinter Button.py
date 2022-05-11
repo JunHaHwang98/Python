8a6d846affade1e87d55c147ac6fb2bd3712aefe
JunHaHwang98 <80338116+JunHaHwang98@users.noreply.github.com>
2022-05-11 20:17:55 +0900

    tkinter button ex

---
+++ tkinter Button.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+b1 = Button(root, text="첫 번째 버튼")
+b2 = Button(root, text="두 번째 버튼")
+b1.pack(side=LEFT)
+b2.pack(side=LEFT)
+
+root.mainloop()
---
+++ tkinter lib.py
@@ -0,0 +1,11 @@
+import tkinter
+
+root = tkinter.Tk()
+
+label = tkinter.Label(root, text="Hello World!")
+label.pack()
+
+button = tkinter.Button(root, text="tkinter로 버튼을 쉽게 만들 수 있습니다.")
+button.pack()
+
+root.mainloop()
